8e24985113a39674b7e0e21a4159d059dda22500
Zakaria Sassi <zakaria.sassi1096@gmail.com>
2024-04-25 23:05:33 +0200

    first commit

---
+++ client/src/App.jsx
@@ -0,0 +1,17 @@
+import body from './assets/body.jpg'
+import Body from './components/Body/Body';
+import Quotatios from './components/Quotatios/Quotatios';
+
+function App() {
+  return (
+    <>
+      <div className='h-full flex w-screen '>
+        <Body/>
+        <Quotatios />
+        
+      </div>
+    </>
+  );
+}
+
+export default App;
